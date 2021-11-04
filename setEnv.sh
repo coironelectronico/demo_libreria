28846773f21605a8880aadec2d67adb2f31c79a4
Agus Scurzi <aguscurzi@yahoo.com.ar>
2021-11-04 11:06:24 -0300

    Update setEnv.sh

--- setEnv.sh
+++ setEnv.sh
@@ -1,5 +1,5 @@
 mkdir env
 echo "export const environment = { sheet_api_key : '$1' }"  > env/env.ts
 - run: sh setEnvironment.sh ${{ secrets.SHEET_API_KEY }}
-echo "export const environment = { book_api_key : '$1' }"  > env/env.ts
+echo "export const environment2 = { book_api_key : '$1' }"  > env/env.ts
 - run: sh setEnvironment.sh ${{ secrets.BOOK_API_KEY }}
